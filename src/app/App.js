1f20b07c98dc1b7d2d7f9ec091f2d22ecf99dc71
Otavio <celani.silva@gmail.com>
2019-09-26 20:03:41 -0300

    modificações na estrutura dos diretórios, inclusão de models e de novos algoritmos

--- src/app/App.js
+++ src/app/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import GameContainer from './views/game/GameContainer';
-import { CustomProvider } from './redux';
+import GameContainer from '../views/game/GameContainer';
+import { CustomProvider } from '../redux';
 
 function App() {
   return (
